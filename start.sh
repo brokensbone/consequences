74fae4a1db65ed39117cc687b70258a00dd2efee
edward <admin@alcachofa.faith>
2020-05-01 18:46:56 +0000

    Prod

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml build --build-arg network=host
 docker-compose -f docker-compose.prod.yml up -d
 \
